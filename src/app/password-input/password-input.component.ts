5f13b5a404fa09fbc54b40fa4b079ad1fb7596bb
lord2025 <90261133+lord2025@users.noreply.github.com>
2023-07-09 19:22:39 +0300

    update logic

--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 export enum PasswordStrength {
+  None = 'none',
   Easy = 'easy',
   Medium = 'medium',
   Strong = 'strong',
@@ -16,34 +17,66 @@ export class PasswordInputComponent {
   passwordStrength: string = '';
 
   checkPasswordStrength() {
-    var passwordStrengthScore = 0;
-
-    if (this.password.length === 0) {
-      this.passwordStrength = '';
-    } else if (this.password.length < 8) {
-      this.passwordStrength = PasswordStrength.Easy;
-    } else {
-      if (/[a-z]/.test(this.password) || /[A-Z]/.test(this.password)) {
-        passwordStrengthScore++;
-      }
-      if (/[0-9]/.test(this.password)) {
-        passwordStrengthScore++;
-      }
-      if (/[^a-zA-Z0-9]/.test(this.password)) {
-        passwordStrengthScore++;
-      }
-    }
-
-    this.updatePasswordStrength(passwordStrengthScore);
+    var passwordStrengthScore = this.getPasswordScore(this.password)
+    this.passwordStrength = this.getPasswordStrength(passwordStrengthScore)
   }
 
-  updatePasswordStrength(passwordStrengthScore: number) {
-    if (passwordStrengthScore === 1) {
-      this.passwordStrength = PasswordStrength.Easy;
-    } else if (passwordStrengthScore === 2) {
-      this.passwordStrength = PasswordStrength.Medium;
-    } else if (passwordStrengthScore === 3) {
-      this.passwordStrength = PasswordStrength.Strong;
+  isPasswordNone(): boolean {
+    return this.passwordStrength == PasswordStrength.None;
+  }
+
+  isPasswordEasy(): boolean {
+    return this.passwordStrength == PasswordStrength.Easy;
+  }
+
+  isPasswordMedium(): boolean {
+    return this.passwordStrength == PasswordStrength.Medium;
+  }
+
+  isPasswordStrong(): boolean {
+    return this.passwordStrength == PasswordStrength.Strong;
+  }
+
+  shouldBeMarkedAsEasy(): boolean {
+    return (this.password.length < 8 && this.password.length !== 0)
+  }
+
+  private getPasswordScore(password: string): number {
+    if (this.password.length === 0) {
+      return 0
+    } else if (this.password.length < 8) {
+      return 1
+    }
+
+    var score = 0
+
+    if (/[A-я]/.test(this.password)) {
+      score++;
+    }
+    if (/[0-9]/.test(this.password)) {
+      score++;
+    }
+    if (/[^A-я0-9]/.test(this.password)) {
+      score++;
+    }
+
+    return score
+  }
+
+  private getPasswordStrength(passwordStrengthScore: number): PasswordStrength {
+    switch (passwordStrengthScore) {
+      case 1: {
+        return PasswordStrength.Easy;
+      }
+      case 2: {
+        return PasswordStrength.Medium;
+      }
+      case 3: {
+        return PasswordStrength.Strong;
+      }
+      default: {
+        return PasswordStrength.None;
+      }
     }
   }
 }
